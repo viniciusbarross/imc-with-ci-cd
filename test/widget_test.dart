e415ac7947ff06d80a64541aa97b931c67ec57e6
vinicius barros <viniciusbarrosipo@gmail.com>
2025-07-16 18:25:53 -0300

    test: remove prints on the code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,27 +7,20 @@ import 'package:provider/provider.dart';
 void main() {
   testWidgets('Test initial validation of insert data page',
       (WidgetTester tester) async {
-    print('Starting test: Test initial validation of insert data page');
     await tester.pumpWidget(
       ChangeNotifierProvider(
         create: (context) => BMIController(),
         child: const MyApp(),
       ),
     );
-    print('Widget pumped');
 
     expect(find.text("Please, enter the weight!"), findsNothing);
     expect(find.text("Please, enter the height!"), findsNothing);
-    print('Initial validation checks passed');
 
     await tester.tap(find.byType(ElevatedButton));
     await tester.pump();
-    print('Button tapped and pumped');
 
     expect(find.text("Please, enter the weight!"), findsOneWidget);
     expect(find.text("Please, enter the height!"), findsOneWidget);
-    print('Validation messages appeared as expected');
-
-    print('Test completed successfully');
   });
 }
